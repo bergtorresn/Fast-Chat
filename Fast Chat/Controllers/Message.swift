fee4f18723d9c2dde84139ebfa1bb8009a44c0f2
Rosemberg Torres Nunes <bergtorres@Mac-mini-de-Rosemberg-2.local>
2018-06-03 19:15:48 -0300

    Model Message

---
+++ Fast Chat/Controllers/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Fast Chat
+//
+//  Created by Rosemberg Torres Nunes on 03/06/2018.
+//  Copyright © 2018 Rosemberg Torres Nunes. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    var sender : String = ""
+    var messageBody : String = ""
+}
